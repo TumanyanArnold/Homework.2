ca2d35f6e1197e9b01605cbee9f6daf2d55527dd
TumanyanArnold <tumanyan2015@list.ru>
2024-08-29 22:47:47 +0300

    Add files via upload

---
+++ Homework.2.py
@@ -0,0 +1,12 @@
+example = 'Топинамбур'
+#1
+print (example[0])
+#2
+print (example[9])
+#3
+print (example[5:10 ])
+#4
+print(example [::-1])
+print(''.join(reversed(example)))
+#5
+print(example[1:10:2])     #+example[])
